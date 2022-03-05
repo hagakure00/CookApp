eb1b54e53ba815f6050bfdbd7f8744af74b618e5
hagakure00 <59146787+hagakure00@users.noreply.github.com>
2022-03-05 20:57:00 +0100

    1.6

--- lib/Widgets/Switch_Image.dart
+++ lib/Widgets/Switch_Image.dart
@@ -1,43 +1,472 @@
 import 'package:flutter/material.dart';
 
 StatelessWidget getImageIngredient(String ingredient) {
-  if (ingredient.contains('poireau')) {
-    return const ImageIngredient(valueImage: "poireau");
-  } else if (ingredient.contains('lardon')) {
-    return const ImageIngredient(valueImage: "lardon");
-  } else if (ingredient.contains('concombre')) {
-    return const ImageIngredient(valueImage: "concombre");
-  } else if (ingredient.contains('comcombre')) {
-    return const ImageIngredient(valueImage: "concombre");
-  }else if (ingredient.contains('basilic')) {
+  if (ingredient.contains('ail')) {
+    return const ImageIngredient(valueImage: "ail");
+  }
+  else if (ingredient.contains('aubergine')) {
+    return const ImageIngredient(valueImage: "aubergine");
+  }
+  else if (ingredient.contains('badiane')) {
+    return const ImageIngredient(valueImage: "badiane");
+  }
+  else if (ingredient.contains('balsamique')) {
+    return const ImageIngredient(valueImage: "balsamique");
+  }
+  else if (ingredient.contains('basilic')) {
     return const ImageIngredient(valueImage: "basilic");
-  } else if (ingredient.contains('vinaigre')) {
-    return const ImageIngredient(valueImage: "vinaigrette");
-  } else if (ingredient.contains('vinaigrette')) {
-    return const ImageIngredient(valueImage: "vinaigrette");
-  }  else if (ingredient.contains('oeuf')) {
-    return const ImageIngredient(valueImage: "oeuf");
-  } else if (ingredient.contains('sel')) {
-    return const ImageIngredient(valueImage: "sel");
-  } else if (ingredient.contains('poivre')) {
-    return const ImageIngredient(valueImage: "poivre");
-  } else if (ingredient.contains('fromage')) {
-    return const ImageIngredient(valueImage: "fromage");
-  } else if (ingredient.contains('courgette')) {
-    return const ImageIngredient(valueImage: "courgette");
-  } else if (ingredient.contains('chocolat')) {
-    return const ImageIngredient(valueImage: "chocolat");
-  } else if (ingredient.contains('sucre')) {
-    return const ImageIngredient(valueImage: "sucre");
-  } else if (ingredient.contains('beurre')) {
+  }
+  else if (ingredient.contains('beurre')) {
     return const ImageIngredient(valueImage: "beurre");
-  }else if (ingredient.contains('olive')) {
-    return const ImageIngredient(valueImage: "olive");
-  } else if (ingredient.contains('chevre')) {
+  }
+  else if (ingredient.contains('bière brune',)) {
+    return const ImageIngredient(valueImage: "biere brune");
+  }
+  else if (ingredient.contains('boeuf')) {
+    return const ImageIngredient(valueImage: "boeuf");
+  }
+  else if (ingredient.contains('bouillon de poule')) {
+    return const ImageIngredient(valueImage: "bouillon de poule");
+  }
+  else if (ingredient.contains('bouquet')) {
+    return const ImageIngredient(valueImage: "bouquet");
+  }
+  else if (ingredient.contains('butternut')) {
+    return const ImageIngredient(valueImage: "butternut");
+  }
+  else if (ingredient.contains('calamar')) {
+    return const ImageIngredient(valueImage: "calamar");
+  }
+  else if (ingredient.contains('canard')) {
+    return const ImageIngredient(valueImage: "canard");
+  }
+  else if (ingredient.contains('cannelle')) {
+    return const ImageIngredient(valueImage: "cannelle");
+  }
+  else if (ingredient.contains('capre')) {
+    return const ImageIngredient(valueImage: "capre");
+  }
+  else if (ingredient.contains('carotte')) {
+    return const ImageIngredient(valueImage: "carotte");
+  }
+  else if (ingredient.contains('cassonade')) {
+    return const ImageIngredient(valueImage: "cassonade");
+  }
+  else if (ingredient.contains('cayenne')) {
+    return const ImageIngredient(valueImage: "cayenne");
+  }
+  else if (ingredient.contains('celeri')) {
+    return const ImageIngredient(valueImage: "celeri");
+  }
+  else if (ingredient.contains('chair a saucisse')) {
+    return const ImageIngredient(valueImage: "chair a saucisse");
+  }
+  else if (ingredient.contains('champignon')) {
+    return const ImageIngredient(valueImage: "champignon");
+  }
+  else if (ingredient.contains('chapelure')) {
+    return const ImageIngredient(valueImage: "chapelure");
+  }
+  else if (ingredient.contains('cheddar')) {
+    return const ImageIngredient(valueImage: "cheddar");
+  }
+  else if (ingredient.contains('chèvre')) {
     return const ImageIngredient(valueImage: "chevre");
-  } else if (ingredient.contains('olive')) {
+  }
+  else if (ingredient.contains('chevre')) {
+    return const ImageIngredient(valueImage: "chevre");
+  }
+  else if (ingredient.contains('chili')) {
+    return const ImageIngredient(valueImage: "chili");
+  }
+  else if (ingredient.contains('chocolat')) {
+    return const ImageIngredient(valueImage: "chocolat");
+  }
+  else if (ingredient.contains('chorizo')) {
+    return const ImageIngredient(valueImage: "chorizo");
+  }
+  else if (ingredient.contains('chou chinois')) {
+    return const ImageIngredient(valueImage: "chou chinois");
+  }
+  else if (ingredient.contains('choucroute')) {
+    return const ImageIngredient(valueImage: "choucroute");
+  }
+  else if (ingredient.contains('ciboulette')) {
+    return const ImageIngredient(valueImage: "ciboulette");
+  }
+  else if (ingredient.contains('citron vert')) {
+    return const ImageIngredient(valueImage: "citron vert");
+  }
+  else if (ingredient.contains('citron')) {
+    return const ImageIngredient(valueImage: "citron");
+  }
+  else if (ingredient.contains('cognac')) {
+    return const ImageIngredient(valueImage: "cognac");
+  }
+  else if (ingredient.contains('colorant')) {
+    return const ImageIngredient(valueImage: "colorant");
+  }
+  else if (ingredient.contains('concombre')) {
+    return const ImageIngredient(valueImage: "concombre");
+  }
+  else if (ingredient.contains('courgette')) {
+    return const ImageIngredient(valueImage: "courgette");
+  }
+  else if (ingredient.contains('coquille')) {
+    return const ImageIngredient(valueImage: "coquille");
+  }
+  else if (ingredient.contains('coriandre')) {
+    return const ImageIngredient(valueImage: "coriandre");
+  }
+  else if (ingredient.contains('cornichon')) {
+    return const ImageIngredient(valueImage: "cornichon");
+  }
+  else if (ingredient.contains('courgette')) {
+    return const ImageIngredient(valueImage: "courgette");
+  }
+  else if (ingredient.contains('crème')) {
+    return const ImageIngredient(valueImage: "creme");
+  }
+  else if (ingredient.contains('creme')) {
+    return const ImageIngredient(valueImage: "creme");
+  }
+  else if (ingredient.contains('crevette')) {
+    return const ImageIngredient(valueImage: "crevette");
+  }
+  else if (ingredient.contains('cube de bouillon')) {
+    return const ImageIngredient(valueImage: "cube de bouillon");
+  }
+  else if (ingredient.contains('cuisse de poulet')) {
+    return const ImageIngredient(valueImage: "cuisse poulet");
+  }
+  else if (ingredient.contains('cumin')) {
+    return const ImageIngredient(valueImage: "cumin");
+  }
+  else if (ingredient.contains('curry')) {
+    return const ImageIngredient(valueImage: "curry");
+  }
+  else if (ingredient.contains('eau')) {
+    return const ImageIngredient(valueImage: "eau");
+  }
+  else if (ingredient.contains('échalote')) {
+    return const ImageIngredient(valueImage: "echalotes");
+  }
+  else if (ingredient.contains('echalote')) {
+    return const ImageIngredient(valueImage: "echalotes");
+  }
+  else if (ingredient.contains('emmental')) {
+    return const ImageIngredient(valueImage: "emmantal");
+  }
+  else if (ingredient.contains('endive')) {
+    return const ImageIngredient(valueImage: "endive");
+  }
+  else if (ingredient.contains('epice')) {
+    return const ImageIngredient(valueImage: "epice");
+  }
+  else if (ingredient.contains('épice')) {
+    return const ImageIngredient(valueImage: "epice");
+  }
+  else if (ingredient.contains('farine')) {
+    return const ImageIngredient(valueImage: "farine");
+  }
+  else if (ingredient.contains('feve')) {
+    return const ImageIngredient(valueImage: "feve");
+  }
+  else if (ingredient.contains('filet de poulet')) {
+    return const ImageIngredient(valueImage: "filet poulet");
+  }
+  else if (ingredient.contains('fleur de sel')) {
+    return const ImageIngredient(valueImage: "fleur de sel");
+  }
+  else if (ingredient.contains('fond de veau')) {
+    return const ImageIngredient(valueImage: "fond de veau");
+  }
+  else if (ingredient.contains('fromage')) {
+    return const ImageIngredient(valueImage: "fromage");
+  }
+  else if (ingredient.contains('genièvre')) {
+    return const ImageIngredient(valueImage: "genievre");
+  }
+  else if (ingredient.contains('gigot')) {
+    return const ImageIngredient(valueImage: "gigot");
+  }
+  else if (ingredient.contains('gingembre')) {
+    return const ImageIngredient(valueImage: "gingembre");
+  }
+  else if (ingredient.contains('girofle')) {
+    return const ImageIngredient(valueImage: "girofle");
+  }
+  else if (ingredient.contains('graisse')) {
+    return const ImageIngredient(valueImage: "graisse");
+  }
+  else if (ingredient.contains('haché')) {
+    return const ImageIngredient(valueImage: "hachée");
+  }
+  else if (ingredient.contains('hamburger')) {
+    return const ImageIngredient(valueImage: "hamburger");
+  }
+  else if (ingredient.contains('haricot rouge')) {
+    return const ImageIngredient(valueImage: "haricot rouge");
+  }
+  else if (ingredient.contains('haricot vert')) {
+    return const ImageIngredient(valueImage: "haricot vert");
+  }
+  else if (ingredient.contains('harissa')) {
+    return const ImageIngredient(valueImage: "harissa");
+  }
+  else if (ingredient.contains('herbe')) {
+    return const ImageIngredient(valueImage: "herbe");
+  }
+  else if (ingredient.contains('huile')) {
+    return const ImageIngredient(valueImage: "huile");
+  }
+  else if (ingredient.contains('jambon')) {
+    return const ImageIngredient(valueImage: "jambon");
+  }
+  else if (ingredient.contains('jaune d\'oeuf')) {
+    return const ImageIngredient(valueImage: "jaune oeuf");
+  }
+  else if (ingredient.contains('ketchup')) {
+    return const ImageIngredient(valueImage: "ketchup");
+  }
+  else if (ingredient.contains('lait de coco')) {
+    return const ImageIngredient(valueImage: "lait coco");
+  }
+  else if (ingredient.contains('lait')) {
+    return const ImageIngredient(valueImage: "lait");
+  }
+  else if (ingredient.contains('laitue')) {
+    return const ImageIngredient(valueImage: "laitue");
+  }
+  else if (ingredient.contains('lapin')) {
+    return const ImageIngredient(valueImage: "lapin");
+  }
+  else if (ingredient.contains('lard')) {
+    return const ImageIngredient(valueImage: "lard");
+  }
+  else if (ingredient.contains('lardon')) {
+    return const ImageIngredient(valueImage: "lardon");
+  }
+  else if (ingredient.contains('lasagne')) {
+    return const ImageIngredient(valueImage: "lasagne");
+  }
+  else if (ingredient.contains('laurier')) {
+    return const ImageIngredient(valueImage: "laurier");
+  }
+  else if (ingredient.contains('lentille')) {
+    return const ImageIngredient(valueImage: "lentille");
+  }
+  else if (ingredient.contains('levure chimique')) {
+    return const ImageIngredient(valueImage: "levure chimique");
+  }
+  else if (ingredient.contains('levure fraiche')) {
+    return const ImageIngredient(valueImage: "levure fraiche");
+  }
+  else if (ingredient.contains('levure de boulanger')) {
+    return const ImageIngredient(valueImage: "levure fraiche");
+  }
+  else if (ingredient.contains('mais')) {
+    return const ImageIngredient(valueImage: "mais");
+  }
+  else if (ingredient.contains('mayonnaise')) {
+    return const ImageIngredient(valueImage: "mayonnaise");
+  }
+  else if (ingredient.contains('menthe')) {
+    return const ImageIngredient(valueImage: "menthe");
+  }
+  else if (ingredient.contains('miel')) {
+    return const ImageIngredient(valueImage: "miel");
+  }
+  else if (ingredient.contains('moule')) {
+    return const ImageIngredient(valueImage: "moule");
+  }
+  else if (ingredient.contains('moutarde')) {
+    return const ImageIngredient(valueImage: "moutarde");
+  }
+  else if (ingredient.contains('muscade')) {
+    return const ImageIngredient(valueImage: "muscade");
+  }
+  else if (ingredient.contains('navet')) {
+    return const ImageIngredient(valueImage: "navet");
+  }
+  else if (ingredient.contains('noisette')) {
+    return const ImageIngredient(valueImage: "noisette");
+  }
+  else if (ingredient.contains('noix coco')) {
+    return const ImageIngredient(valueImage: "noix");
+  }
+  else if (ingredient.contains('nouille de riz')) {
+    return const ImageIngredient(valueImage: "nouille de riz");
+  }
+  else if (ingredient.contains('nuoc mam')) {
+    return const ImageIngredient(valueImage: "nuoc mam");
+  }
+  else if (ingredient.contains('oeuf')) {
+    return const ImageIngredient(valueImage: "oeuf");
+  }
+  else if (ingredient.contains('oignon')) {
+    return const ImageIngredient(valueImage: "oignon");
+  }
+  else if (ingredient.contains('oignon blanc')) {
+    return const ImageIngredient(valueImage: "oignon blanc");
+  }
+  else if (ingredient.contains('oignon nouveau')) {
+    return const ImageIngredient(valueImage: "oignon new");
+  }
+  else if (ingredient.contains('olive noir')) {
+    return const ImageIngredient(valueImage: "olive noir");
+  }
+  else if (ingredient.contains('olive')) {
     return const ImageIngredient(valueImage: "olive");
   }
+  else if (ingredient.contains('pain de mie')) {
+    return const ImageIngredient(valueImage: "pain de mie");
+  }
+  else if (ingredient.contains('pain d\'épice')) {
+    return const ImageIngredient(valueImage: "pain epice");
+  }
+  else if (ingredient.contains('pain d\'epice')) {
+    return const ImageIngredient(valueImage: "pain epice");
+  }
+  else if (ingredient.contains('paprika')) {
+    return const ImageIngredient(valueImage: "parmesan");
+  }
+  else if (ingredient.contains('pate')) {
+    return const ImageIngredient(valueImage: "pate");
+  }
+  else if (ingredient.contains('pâte')) {
+    return const ImageIngredient(valueImage: "pate");
+  }
+  else if (ingredient.contains('paupiettes de veau')) {
+    return const ImageIngredient(valueImage: "paupiettes de veau");
+  }
+  else if (ingredient.contains('paupiette de veau')) {
+    return const ImageIngredient(valueImage: "paupiettes de veau");
+  }
+  else if (ingredient.contains('persil')) {
+    return const ImageIngredient(valueImage: "persil");
+  }
+  else if (ingredient.contains('petit pois')) {
+    return const ImageIngredient(valueImage: "petit pois");
+  }
+  else if (ingredient.contains('piment')) {
+    return const ImageIngredient(valueImage: "piment");
+  }
+  else if (ingredient.contains('poireau')) {
+    return const ImageIngredient(valueImage: "poireau");
+  }
+  else if (ingredient.contains('pois chiche')) {
+    return const ImageIngredient(valueImage: "pois chiche");
+  }
+  else if (ingredient.contains('poivre')) {
+    return const ImageIngredient(valueImage: "poivre");
+  }
+  else if (ingredient.contains('poivron')) {
+    return const ImageIngredient(valueImage: "poivron");
+  }
+  else if (ingredient.contains('pomme de terre')) {
+    return const ImageIngredient(valueImage: "pomme de terre");
+  }
+  else if (ingredient.contains('porc')) {
+    return const ImageIngredient(valueImage: "porc");
+  }
+  else if (ingredient.contains('poulet')) {
+    return const ImageIngredient(valueImage: "poulet");
+  }
+  else if (ingredient.contains('purée')) {
+    return const ImageIngredient(valueImage: "puree");
+  }
+  else if (ingredient.contains('ramen')) {
+    return const ImageIngredient(valueImage: "ramen");
+  }
+  else if (ingredient.contains('reblochon')) {
+    return const ImageIngredient(valueImage: "reblochon");
+  }
+  else if (ingredient.contains('riesling')) {
+    return const ImageIngredient(valueImage: "riesling");
+  }
+  else if (ingredient.contains('riz')) {
+    return const ImageIngredient(valueImage: "riz");
+  }
+  else if (ingredient.contains('romarin')) {
+    return const ImageIngredient(valueImage: "romarin");
+  }
+  else if (ingredient.contains('safran')) {
+    return const ImageIngredient(valueImage: "safran");
+  }
+  else if (ingredient.contains('salade')) {
+    return const ImageIngredient(valueImage: "salade");
+  }
+  else if (ingredient.contains('sauce soja')) {
+    return const ImageIngredient(valueImage: "sauce soja");
+  }
+  else if (ingredient.contains('saucisse de strasbourg')) {
+    return const ImageIngredient(valueImage: "saucisse strasbourg");
+  }
+  else if (ingredient.contains('saucisse')) {
+    return const ImageIngredient(valueImage: "saucisse");
+  }
+  else if (ingredient.contains('saumon')) {
+    return const ImageIngredient(valueImage: "saumon");
+  }
+  else if (ingredient.contains('sel')) {
+    return const ImageIngredient(valueImage: "sel");
+  }
+  else if (ingredient.contains('soja')) {
+    return const ImageIngredient(valueImage: "soja");
+  }
+  else if (ingredient.contains('steack de boeuf')) {
+    return const ImageIngredient(valueImage: "steack boeuf");
+  }
+  else if (ingredient.contains('sucre glace')) {
+    return const ImageIngredient(valueImage: "sucre glace");
+  }
+  else if (ingredient.contains('sucre semoule')) {
+    return const ImageIngredient(valueImage: "sucre semoule");
+  }
+  else if (ingredient.contains('sucre')) {
+    return const ImageIngredient(valueImage: "sucre");
+  }
+  else if (ingredient.contains('tabasco')) {
+    return const ImageIngredient(valueImage: "tabasco");
+  }
+  else if (ingredient.contains('tagliatelle')) {
+    return const ImageIngredient(valueImage: "tagliatelles");
+  }
+  else if (ingredient.contains('thon')) {
+    return const ImageIngredient(valueImage: "thon");
+  }
+  else if (ingredient.contains('thym')) {
+    return const ImageIngredient(valueImage: "thym");
+  }
+  else if (ingredient.contains('tomate cerise')) {
+    return const ImageIngredient(valueImage: "tomate cerise");
+  }
+  else if (ingredient.contains('tomate')) {
+    return const ImageIngredient(valueImage: "tomate");
+  }
+  else if (ingredient.contains('tortillas')) {
+    return const ImageIngredient(valueImage: "tortillas");
+  }
+  else if (ingredient.contains('vanille')) {
+    return const ImageIngredient(valueImage: "vanille");
+  }
+  else if (ingredient.contains('veau')) {
+    return const ImageIngredient(valueImage: "veau");
+  }
+  else if (ingredient.contains('vin blanc')) {
+    return const ImageIngredient(valueImage: "vin blanc");
+  }
+  else if (ingredient.contains('vin rouge')) {
+    return const ImageIngredient(valueImage: "vin rouge");
+  }
+  else if (ingredient.contains('vinaigrette')) {
+    return const ImageIngredient(valueImage: "vinaigrette");
+  }
+  else if (ingredient.contains('yakitori')) {
+    return const ImageIngredient(valueImage: "yakitori");
+  }
+
 
 
   else {
